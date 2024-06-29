212228f8b4690efc0c7ef47eac363e1dbec8f8b1
tomiliudev <tomiliudev@yahoo.co.jp>
2024-06-30 00:01:17 +0900

    Animate.css導入

--- src/components/AnimationTest.vue
+++ src/components/AnimationTest.vue
@@ -5,29 +5,11 @@ const isShow = ref(true)
 <template>
   <button @click="isShow = !isShow">isShow</button>
 
-  <Transition name="fade">
+  <Transition
+    enter-active-class="animate__animated animate__shakeX"
+    leave-active-class="animate__animated animate__flash"
+  >
     <div v-if="isShow">Hello</div>
   </Transition>
 </template>
-<style scoped>
-/* 表示する時のアニメーション */
-.fade-enter-from {
-  opacity: 0;
-}
-.fade-enter-to {
-  opacity: 1;
-}
-.fade-enter-active {
-  transition: opacity 1s;
-}
-/* 非表示する時のアニメーション */
-.fade-leave-from {
-  opacity: 1;
-}
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-leave-active {
-  transition: opacity 1s;
-}
-</style>
+<style scoped></style>
