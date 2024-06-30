7f2d12871a6c9d761cce55321caa48e556287c11
tomiliudev <tomiliudev@yahoo.co.jp>
2024-07-01 00:16:50 +0900

    javascriptでrouterを使ってページ遷移する方法

--- src/components/vueRouterTest/RouterTest.vue
+++ src/components/vueRouterTest/RouterTest.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+function toAbout() {
+  router.push({ path: '/about' }) // 履歴残る。ブラウザの戻るボタンで前の画面へ
+  //router.replace({ path: '/about' }) // 履歴残らない。ブラウザの戻るボタンで前の画面へ行かなくなる
+  //router.go(-1) // 一個前の履歴へ戻る
+  //router.go(1) // 一個先の履歴へ
+}
+</script>
 
 <template>
   <RouterLink to="/?lang=ja#aaa">Home</RouterLink> |
-  <RouterLink :to="{ path: '/about', query: { lang: 'ja' }, hash: '#bbb' }">About</RouterLink>
+  <RouterLink :to="{ path: '/about', query: { lang: 'ja' }, hash: '#bbb' }">About</RouterLink> |
+  <button @click="toAbout">About2</button>
   <RouterView />
 </template>
