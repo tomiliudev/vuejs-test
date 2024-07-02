2d3e60679f3b63c4834aec483c2b225406814c7e
tomiliudev <tomiliudev@yahoo.co.jp>
2024-07-02 23:34:17 +0900

    Not Foundページ。リダイレクト設定

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,24 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from '@/components/vueRouterTest/HomeView.vue'
 import AboutView from '@/components/vueRouterTest/AboutView.vue'
 import BlogView from '@/components/vueRouterTest/BlogView.vue'
+import NotFound from '@/components/vueRouterTest/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/about', name: 'about', component: AboutView },
-  { path: '/blog/:id/:version', name: 'blog', component: BlogView }
+  { path: '/blog/:id/:version', name: 'blog', component: BlogView },
   //   { path: '/blog/id-:id-number', name: 'blog', component: BlogView } // :id以外の部分は必須
   //   { path: '/blog/:id?', name: 'blog', component: BlogView } // パラメータを付けても付けなくてもいい
   //   { path: '/blog/:id+', name: 'blog', component: BlogView } // 1/2/3のようなURLをつくることができる
   //   { path: '/blog/:id*', name: 'blog', component: BlogView } // ?と+の合体版
   //   { path: '/blog/:id(\\d+)', name: 'blog', component: BlogView } // ()内は正規表現、例は数字が１文字以上
   //   { path: '/blog/:id(.+)', name: 'blog', component: BlogView } // ()内は正規表現、例は任意の文字が１文字以上
+  {
+    path: '/:catchAll(.*)*', // すべてのパスを受け付ける。パスの評価優先度はより詳細度の高いパスが優先される
+    name: 'notFound',
+    redirect: { name: 'home' }, // リダイレクト先。これを指定した場合、componentが無視される
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
