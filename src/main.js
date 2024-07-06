9fc517a7d39c0963b2b2d05724dfa8affffe3627
tomiliudev <tomiliudev@yahoo.co.jp>
2024-07-07 01:05:40 +0900

    Piniaをmain.jsでの使い方

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
+import { createApp, watchEffect } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index'
+import { useCounterStore } from '@/stores/counter'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+
+// main.jsでPiniaを使う場合は app.use(pinia)の後でやる必要がある
+const counterStore = useCounterStore()
+watchEffect(() => console.log('main.js', counterStore.counter))
+
 app.mount('#app')
